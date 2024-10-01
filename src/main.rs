008727d2f34b1520bc3b4fb0b450666959af5db6
Damian Figiel <damian.figiel@enjin.io>
2024-10-01 10:15:33 +0200

    Basic Actix Web server with initial route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to Decentralized API Gateway!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Decentralized API Gateway is listening on port 9090");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:9090")?
+    .run()
+    .await
 }
